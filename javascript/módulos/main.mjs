c4d0b020411bcbc4a73d0b6089731ecb96e7f226
wesley7777 <wesleysantosaguiar@hotmail.com>
2022-06-24 01:55:54 -0300

    subindo aula de módulos

---
+++ javascript/módulos/main.mjs
@@ -0,0 +1,5 @@
+import { mostraCidade, mostraIdade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade("Wesley", 22));
+console.log(mostraCidade("Wesley", 'São Paulo'));
+console.log(mostraIdade("Wesley", 'Estudar'));
